6cead2b960e4d4fca12ffe5a406cc918550925ee
KseniaLF <85734272+KseniaLF@users.noreply.github.com>
2023-04-11 15:08:16 +0000

    Deploying to gh-pages from @ KseniaLF/find-books@d35f46384ff4921bbbbf25bdea4bce91b6556e56 🚀

---
+++ static/js/501.7af7f1fd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[501],{6501:function(e,t,n){n.r(t);var o=n(184);t.default=function(){return(0,o.jsxs)("main",{children:[(0,o.jsx)("p",{children:"Hi, this is your collection"}),"no books in collection"]})}}}]);
+//# sourceMappingURL=501.7af7f1fd.chunk.js.map
